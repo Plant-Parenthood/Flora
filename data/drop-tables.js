c6664a88cbef16fb697709845197b8e9f2173d41
drmeloy <drmeloy@gmail.com>
2019-11-18 11:32:18 -0700

    lots of initial scaffolding, be sure to modify files that were copy pasted

---
+++ data/drop-tables.js
@@ -0,0 +1,29 @@
+const client = require('../lib/client');
+
+run();
+
+async function run() {
+
+    // !!!
+    // STILL TO DO: DROP TABLES SPECIFIC FOR THE PLANT APP! DELETE THIS LINE ONCE PLANT APP SPECIFIC DROP TABLES HAVE BEEN MADE!!!
+    // !!!
+    
+    try {
+        // run a query to create tables
+        await client.query(`
+            DROP TABLE IF EXISTS favorites;
+            DROP TABLE IF EXISTS users;
+        `);
+
+        console.log('drop tables complete');
+    }
+    catch (err) {
+        // problem? let's see the error...
+        console.log(err);
+    }
+    finally {
+        // success or failure, need to close the db connection
+        client.end();
+    }
+    
+}

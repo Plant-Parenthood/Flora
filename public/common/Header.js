6a80e23d8a1fba2ca46db85bf8636aaef0c92a86
Brittany Houtz <brittany.houtz@gmail.com>
2019-11-20 10:07:51 -0800

    header css

--- public/common/Header.js
+++ public/common/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
 
         return /*html*/`
             <header>
-                <img class="logo" src="./assets/hike-icon.jpg" alt="Hike Logo">
+                <img class="logo" src="./assets/hike-icon-white.png" alt="Hike Logo">
                 <h1>${title}</h1>
                 <button class="log-out">Log out!</button>
             </header>

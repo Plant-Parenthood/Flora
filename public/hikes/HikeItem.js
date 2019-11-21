a0e967275bfe467f3b4ae451af62d6db569acd6a
Tali Avni <tali@aristo7.com>
2019-11-21 15:42:15 -0800

    merge conflicts

--- public/hikes/HikeItem.js
+++ public/hikes/HikeItem.js
@@ -1,9 +1,5 @@
 import Component from '../Component.js';
-<<<<<<< HEAD
-import { makeFavorite, unFavorite, saveOrFetchHike, getCampgrounds } from '../services/hikes-api.js';
-=======
 import { makeFavorite, unFavorite, saveOrFetchHike, getCampgrounds, getWeather } from '../services/hikes-api.js';
->>>>>>> c16731a24edf3342f6edcc2ea7943428f67a0213
 
 
 
@@ -14,14 +10,9 @@ class HikeItem extends Component {
 
         //Favorite functionality same as source- SHOULD WE CHANGE?
         const removeUnFavorites = this.props.removeUnFavorites;
-<<<<<<< HEAD
-        const favoriteButton = li.querySelector('.favorite-star');
-        const infoButton = li.querySelector('.info-button');
-=======
         const favoriteButton = li.querySelector('.favorite-heart');
         const infoButton = li.querySelector('.info-button');
 
->>>>>>> c16731a24edf3342f6edcc2ea7943428f67a0213
         favoriteButton.addEventListener('click', async() => {
             hike.isFavorite = !hike.isFavorite;
 
@@ -45,13 +36,10 @@ class HikeItem extends Component {
 
         infoButton.addEventListener('click', async() => {
             const campgrounds = await getCampgrounds(hike.latitude, hike.longitude);
-<<<<<<< HEAD
             console.log('SHOULD BE OUR CAMPGROUNDS', campgrounds);
             renderModal(hike, campgrounds);
-=======
             const weather = await getWeather(hike.latitude, hike.longitude);
             console.log(weather, 'weather');
->>>>>>> c16731a24edf3342f6edcc2ea7943428f67a0213
         });
     }
 
@@ -68,23 +56,15 @@ class HikeItem extends Component {
             'blueBlack': 'Hard',
             'black': 'Hardest'
         };
- 
+
         return /*html*/`
             <li class="hike-item">
-<<<<<<< HEAD
-
-                    <button class="favorite-star ${starClass}">❤</button>
-                    <button class="info-button">i</button>
-                    <a href="${hike.url}" class="hike-name"><img src="${hike.imgMedium}" onerror="this.onerror=null;this.src='/assets/placeholder-image.png';" alt="${hike.name}">${hike.name}</a>
-                
-=======
                 <section class="fav-info">
                     <button class="info-button">ⓘ</button>
                     <button class="favorite-heart ${heartClass}">❤</button>
                 </section>
                     <img src="${hike.imgMedium}" onerror="this.onerror=null;this.src='/assets/placeholder-image.png';">
                     <h2 class="hike-name">${hike.name}</h2>
->>>>>>> c16731a24edf3342f6edcc2ea7943428f67a0213
                 <summary>
                     Length: ${hike.length} m.<br>
                     Difficulty: ${difficultyValues[hike.difficulty]}<br>

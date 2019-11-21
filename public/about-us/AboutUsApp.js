e8d191b0fcb223cde9a523f119f9aa29afb5557d
Jodi N Kansagor <joedvice@gmail.com>
2019-11-21 14:27:12 -0800

    more about us work

--- public/about-us/AboutUsApp.js
+++ public/about-us/AboutUsApp.js
@@ -23,32 +23,26 @@ class AboutUsApp extends Component {
                 <main>
                     <ul>
                         <li>
-                            <h2>Brittany<h2>
                             <img src="../assets/brittany_square.jpg" alt="Brittany">
                             <p>Brittany is from Portland, Oregon. When not in front of a computer screen, you can find her reading with her cat and a cup of coffee, training in aerial circus arts, or donating her last dollar to Hollywood Theatre.</p>
                         </li>
                         <li>
-                            <h2>Dan<h2>
                             <img src="../assets/dan_square.jpg" alt="Dan">
                             <p>Business owner, head brewer, hostel manager, wilderness guide, technician, developer; Dan's held many titles over the years, but one thing has stayed the same: his drive to always learn more, move forward, and have fun doing it!</p>
                         </li>
                         <li>
-                            <h2>JBJ<h2>
                             <img src="../assets/jbj_square.jpg" alt="Brittany">
-                            <p>Something Something Something about JBJ. </p>
+                            <p>JBJ </p>
                         </li>
                         <li>
-                            <h2>Lisa<h2>
                             <img src="../assets/lisa_square.jpg" alt="Lisa">
                             <p>Lisa loves all things trails and prefers using physical maps to navigate the great outdoors. She invented the competitive leisure sport of Adventure Tubing, which consists of trail running with inflated innertubes to alpine lakes.</p>
                         </li>
                         <li>
-                            <h2>Tali<h2>
                             <img src="../assets/brittany_square.jpg" alt="Tali">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         </li>
                         <li>
-                            <h2>Tess<h2>
                             <img src="../assets/brittany_square.jpg" alt="Tali">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         </li>

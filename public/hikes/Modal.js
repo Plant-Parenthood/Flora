fd0ea00087099306ed1c566fbd1fd9c6018b6832
drmeloy <drmeloy@gmail.com>
2019-11-21 10:21:36 -0700

    filled out modal

--- public/hikes/Modal.js
+++ public/hikes/Modal.js
@@ -13,16 +13,35 @@ class Modal extends Component {
                 </div>
             `;
         } else {
+            const difficultyValues = {
+                'any': 'All Levels',
+                'green': 'Easiest',
+                'greenBlue': 'Easy',
+                'blue': 'Medium',
+                'blueBlack': 'Hard',
+                'black': 'Hardest'
+            };
+
+        
             return /*html*/ `
-            <div>
-                <a href="${modalHike.url}" class="modalHike-name"><img src="${modalHike.imgMedium}" onerror="this.onerror=null;this.src='/assets/placeholder-image.png';" alt="${modalHike.name}">${modalHike.name}</a>
-                <summary>
-                    Length: ${modalHike.length} m.<br>
-                    Difficulty: ${modalHike.difficulty}<br>
-                    Summary: ${modalHike.summary}
-                </summary>
+            <div class=modal>
+                <h1 class="hike-name">${modalHike.name}</h1>
+                <h3 class="hike-location">${modalHike.location}</h3>
+                <img class="hike-picture" src="${modalHike.imgMedium}" onerror="this.onerror=null;this.src='/assets/placeholder-image.png';" alt="${modalHike.name}">
+                <div class="stars-votes-difficult">
+                    <span class="num-stars">Rating: ${modalHike.stars}⭐s</span><span class="num-votes">Votes: ${modalHike.starVotes}</span><span class="difficulty">Difficulty: ${difficultyValues[modalHike.difficulty]}</span>
+                </div>
+                <p class="hike-summary">${modalHike.summary}</p>
+                <div class="length-high-low">
+                    <span class="length">Length: ${modalHike.length} miles</span><span class="ascent">Vertical ascent: ${modalHike.ascent} feet</span><span class="descent">Vertical descent: ${modalHike.descent} feet</span><span class="high">Max elevation: ${modalHike.high} feet</span><span class="low">Minimum elevation: ${modalHike.low} feet</span>
+                </div>
+                <fieldset class="trail-status">
+                    <legend>Trail conditions as of ${modalHike.conditionDate}</legend>
+                    <p class="condition-status">Trail condition: ${modalHike.conditionStatus}<p>
+                    <p class="condition-details">Condition details: ${modalHike.conditionDetails}</p>
+                </fieldset>
+                <a class="website-link" src="${modalHike.url}" target="_blank">Check out this trail's website!</a>
                 <section>
-                <-- Because we will have many campgrounds we will want another component for campgrounds and do logic to append campground components to the modal component -->
                     Campgrounds: ${JSON.stringify(campgrounds)}
                 </section>
             </div>

8d636d69774ef74db109ee7dab1f684a6ce6d912
Lisa Carpenter <carpenter.lisa@gmail.com>
2019-11-21 16:34:01 -0800

    fixed homepage issue for logged in user

--- public/home/App.js
+++ public/home/App.js
@@ -4,66 +4,80 @@ import Footer from '../common/Footer.js';
 import SignUp from './SignUp.js';
 import SignIn from './SignIn.js';
 import { signUp as userSignUp, signIn as userSignIn } from '../services/hikes-api.js';
+import Nav from '../common/Nav.js';
 
 function success(user) {
     localStorage.setItem('USER', JSON.stringify(user));
-    const searchParams = new URLSearchParams(location.search);
     location = '/hikes.html';
 }
 
 class App extends Component {
 
     onRender(dom) {
+
         const header = new Header();
         dom.prepend(header.renderDOM());
 
-        const errors = dom.querySelector('.errors');
-        const signUpContainer = dom.querySelector('#signup-container');
-        const signInContainer = dom.querySelector('#signin-container');
-        
-        const signUp = new SignUp({
-            onSignUp: async newUser => {
-                errors.textContent = '';
+        const nav = new Nav();
+        dom.appendChild(nav.renderDOM());
 
-                try {
-                    const user = await userSignUp(newUser);
-                    success(user);
-                }
-                catch (err) {
-                    errors.textContent = err;
-                    throw err;
-                }
-            }
-        });
-        signUpContainer.appendChild(signUp.renderDOM());
-        
-        const signIn = new SignIn({
-            onSignIn: async credentials => {
-                errors.textContent = '';
+        let token = '';
+        const json = localStorage.getItem('USER');
+        if (json) {
+            const user = JSON.parse(json);
+            token = user.token;
+        }
 
-                try {
-                    const user = await userSignIn(credentials);
-                    success(user);
-                }
-                catch (err) {
-                    errors.textContent = err;
-                    throw err;
-                }
-            }
-        });
-        signInContainer.appendChild(signIn.renderDOM());
+        if (!token){
+            const errors = dom.querySelector('.errors');
+            const signUpContainer = dom.querySelector('#signup-container');
+            const signInContainer = dom.querySelector('#signin-container');
+            
+            const signUp = new SignUp({
+                onSignUp: async newUser => {
+                    errors.textContent = '';
 
-        const switchToSignIn = dom.querySelector('#signin-button');
-        switchToSignIn.addEventListener('click', () => {
-            signInContainer.classList.remove('no-display');
-            signUpContainer.classList.add('no-display');
-        });
+                    try {
+                        const user = await userSignUp(newUser);
+                        success(user);
+                    }
+                    catch (err) {
+                        errors.textContent = err;
+                        throw err;
+                    }
+                }
+            });
+            signUpContainer.appendChild(signUp.renderDOM());
+            
+            const signIn = new SignIn({
+                onSignIn: async credentials => {
+                    errors.textContent = '';
+
+                    try {
+                        const user = await userSignIn(credentials);
+                        success(user);
+                    }
+                    catch (err) {
+                        errors.textContent = err;
+                        throw err;
+                    }
+                }
+            });
+            signInContainer.appendChild(signIn.renderDOM());
         
-        const switchToSignUp = dom.querySelector('#signup-button');
-        switchToSignUp.addEventListener('click', () => {
-            signUpContainer.classList.remove('no-display');
-            signInContainer.classList.add('no-display');
-        });
+
+            const switchToSignIn = dom.querySelector('#signin-button');
+            switchToSignIn.addEventListener('click', () => {
+                signInContainer.classList.remove('no-display');
+                signUpContainer.classList.add('no-display');
+            });
+            
+            const switchToSignUp = dom.querySelector('#signup-button');
+            switchToSignUp.addEventListener('click', () => {
+                signUpContainer.classList.remove('no-display');
+                signInContainer.classList.add('no-display');
+            });
+        }
 
         const footer = new Footer();
         dom.appendChild(footer.renderDOM());
@@ -71,23 +85,39 @@ class App extends Component {
 
     renderHTML() {
         // TO DO: update what is shown to user based on sign in status
+        const loginHTML = 
+            `<section class="no-display" id="signup-container">
+            <p class="errors"></p>
+                <p class="switch">
+                <button id="signin-button">Existing User Sign-In</button>
+                </p>
+            </section>
+            <section id="signin-container">
+                <p class="switch">
+                <button id="signup-button">New User Registration</button>
+                </p>
+            </section>`;
+
+        const loggedInHTML = `
+        <img class="mthood" src="../assets/mthood.jpeg" alt="Image of Mt. Hood in Oregon">
+        `;
+
+        let token = '';
+        const json = localStorage.getItem('USER');
+        if (json) {
+            const user = JSON.parse(json);
+            token = user.token;
+        }
+        const actualHTMLtoRender = token ? loggedInHTML : loginHTML;
+
+
+
         return /*html*/`
             <div>
                 <!-- header goes here -->
                 <main>
-                    <section class="no-display" id="signup-container">
-                    <p class="errors"></p>
-                        <p class="switch">
-                        <button id="signin-button">Existing User Sign-In</button>
-                        </p>
-                    </section>
-                    <section id="signin-container">
-                        <p class="switch">
-                        <button id="signup-button">New User Registration</button>
-                        </p>
-                    </section>
+                ${actualHTMLtoRender}
                 </main>
-                <!-- footer goes here -->
             </div>
         `;
     }

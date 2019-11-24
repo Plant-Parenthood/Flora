278ac42670d35d6e52b4044868966f876e558461
Brittany Houtz <brittany.houtz@gmail.com>
2019-11-24 13:54:42 -0800

    heart experiment

--- public/hikes/HikeItem.js
+++ public/hikes/HikeItem.js
@@ -59,7 +59,7 @@ class HikeItem extends Component {
             <li class="hike-item">
                 <section class="fav-info">
                     <button class="info-button">ⓘ</button>
-                    <button class="favorite-heart ${heartClass}">♥❤🖤♡</button>
+                    <button class="favorite-heart ${heartClass}">♡</button>
                 </section>
                     <img src="${hike.imgMedium}" onerror="this.onerror=null;this.src='/assets/placeholder-image.png';">
                     <h2 class="hike-name">${hike.name}</h2>
